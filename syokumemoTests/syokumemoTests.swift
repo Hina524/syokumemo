6d4e4b919c6c639f524758b61556147c57d18061
Hina524 <132186412+Hina524@users.noreply.github.com>
2025-05-06 03:30:00 +0900

    first commit

---
+++ syokumemoTests/syokumemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  syokumemoTests.swift
+//  syokumemoTests
+//
+//  Created by KONISHI Hina on 2025/05/06.
+//
+
+import Testing
+
+struct syokumemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
