7e4413580ca47b65184286f50e68aea4b6768027
Hina524 <132186412+Hina524@users.noreply.github.com>
2025-08-22 13:52:52 +0900

    feat: 戻るボタン追加

--- syokumemo/Screens/GraphContentPage.swift
+++ syokumemo/Screens/GraphContentPage.swift
@@ -14,9 +14,33 @@ struct GraphContentPage: View {
     @StateObject private var viewModel = GraphViewModel()
     @State private var rawSelectedDate: Date?
     let ingredient: GetIngredientsAndParchaseHistoryQuery.Data.Ingredient
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         VStack {
+            ZStack {
+                HStack {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        HStack(spacing: 4) {
+                            Image(systemName: "chevron.left")
+                            Text("戻る")
+                                .font(.body)
+                        }
+                        .foregroundColor(.black)
+                    }
+                    Spacer()
+                }
+                
+                Text(ingredient.name)
+                    .font(.headline)
+                    .foregroundColor(.black)
+            }
+            .padding()
+            .frame(height: 50)
+            .navigationBarBackButtonHidden(true)
+            
             if viewModel.isLoading {
                 ProgressView("読み込み中…")
             } else if let error = viewModel.errorMessage {
@@ -24,67 +48,61 @@ struct GraphContentPage: View {
                     .foregroundColor(.red)
             } else {
                 Section(
-                    header: Text(ingredient.name)
-                        .font(.headline)
                 ) {
                     Chart {
                         ForEach(
                             ingredient.purchaseHistory
-                                    .compactMap { history -> LineData? in
-                                        guard let date = DateFormatter.apiFormat.date(from: history.date) else { return nil }
-                                        return LineData(id: history.id, date: date, price: history.price)
-                                    }
-                                    .sorted { $0.date < $1.date }
-                            ) { data in
-                                LineMark(
-                                    x: .value("日付", data.date),
-                                    y: .value("価格", data.price)
+                                .compactMap { history -> LineData? in
+                                    guard let date = DateFormatter.apiFormat.date(from: history.date) else { return nil }
+                                    return LineData(id: history.id, date: date, price: history.price)
+                                }
+                                .sorted { $0.date < $1.date }
+                        ) { data in
+                            LineMark(
+                                x: .value("日付", data.date),
+                                y: .value("価格", data.price)
+                            )
+                            .foregroundStyle(.green)
+                            .symbol(Circle())
+                            if let rawSelectedDate {
+                                // 選択した時にRuleMarkを追加
+                                RuleMark(
+                                    x: .value("Selected", rawSelectedDate, unit: .month)
                                 )
-                                .foregroundStyle(.green)
-                                .symbol(Circle())
-                                if let rawSelectedDate {
-                                    // 選択した時にRuleMarkを追加
-                                    RuleMark(
-                                        x: .value("Selected", rawSelectedDate, unit: .month)
+                                .foregroundStyle(.gray.opacity(0.3))
+                                .offset(yStart: -10)
+                                .zIndex(-1)// RuleMarkがLineMarkの後ろに来るように。defaultは0
+                                .annotation( // 注釈の作成
+                                    position: .top, //
+                                    spacing: 0,
+                                    overflowResolution: .init(
+                                        x: .fit(to: .chart), // X軸では注釈がグラフの端の境界を超えないようにfitさせる
+                                        y: .disabled // Y軸では注釈がグラフのすぐ上に来るようにするようにオーバーフロー解決を無効にする
                                     )
-                                    .foregroundStyle(.gray.opacity(0.3))
-                                    .offset(yStart: -10)
-                                    .zIndex(-1)// RuleMarkがLineMarkの後ろに来るように。defaultは0
-                                    .annotation( // 注釈の作成
-                                        position: .top, //
-                                        spacing: 0,
-                                        overflowResolution: .init(
-                                            x: .fit(to: .chart), // X軸では注釈がグラフの端の境界を超えないようにfitさせる
-                                            y: .disabled // Y軸では注釈がグラフのすぐ上に来るようにするようにオーバーフロー解決を無効にする
-                                        )
-                                    ) {
-                                        Text(rawSelectedDate, format: Date.FormatStyle(date: .numeric, time: .none))
-                                            .padding()
-                                            .background(Color.cyan)
-                                    }
+                                ) {
+                                    Text(rawSelectedDate, format: Date.FormatStyle(date: .numeric, time: .none))
+                                        .padding()
+                                        .background(Color.cyan)
                                 }
                             }
                         }
-                        .frame(height: 200)
-                        .padding(.top, 65)
-                        .chartXSelection(value: $rawSelectedDate)
-                        .chartXAxis {
-                            AxisMarks(values: .automatic(desiredCount: 5)) { value in
-                                if let dateValue = value.as(Date.self) {
-                                    AxisValueLabel {
-                                        Text(dateValue.formattedJapaneseMonth())
-                                    }
+                    }
+                    .frame(height: 200)
+                    .padding(.top, 65)
+                    .chartXSelection(value: $rawSelectedDate)
+                    .chartXAxis {
+                        AxisMarks(values: .automatic(desiredCount: 5)) { value in
+                            if let dateValue = value.as(Date.self) {
+                                AxisValueLabel {
+                                    Text(dateValue.formattedJapaneseMonth())
                                 }
                             }
                         }
+                    }
                 }
-                .navigationTitle(ingredient.name)
-                .navigationBarTitleDisplayMode(.large)
             }
+            Spacer()
         }
-//        .onAppear {
-//            viewModel.fetchGetPriceTrend()
-//        }
     }
 }
 
